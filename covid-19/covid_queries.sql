0dde829433d33b969990c554ba14bfc3d6b721b9
Pedro Vaz <pedromvaz@gmail.com>
2023-10-08 00:55:13 +0100

    Added two more queries to the Covid-19 file, to find which are the locations with the most reported cases of Covid-19 (and respective number of deaths), and which are the locations with the least reported cases

--- covid-19/covid_queries.sql
+++ covid-19/covid_queries.sql
@@ -24,3 +24,30 @@ group by
     i.initial_cases
 order by first_date
 limit 10;
+
+-- find out the top 10 countries with the highest number of cases, and their total deaths
+select
+    l.name as country,
+    max(c.total_cases) as total_cases,
+    max(c.total_deaths) as total_deaths
+from daily_covid_stats c
+inner join location l
+    on c.location_id = l.id
+    and l.continent_id is not null
+group by country
+order by total_cases desc
+limit 10;
+
+-- find out the top 10 countries with the lowest number of cases, and their total deaths
+select
+    l.name as country,
+    max(c.total_cases) as total_cases,
+    max(c.total_deaths) as total_deaths
+from daily_covid_stats c
+inner join location l
+    on c.location_id = l.id
+    and l.continent_id is not null
+group by country
+having total_cases > 0
+order by total_cases asc
+limit 10;
